0b8dda81c20491d7f2786685b5f8929cbc9cc6b1
nbyrd9 <nianibyrd@gmail.com>
2021-04-14 17:03:21 -0400

    commit tree 89b52d1d2927a0d3d660f957f33b0626f25f6927

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards( namesArray, event ) {
+  console.log(namesArray, event)
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+
+function countDown( startingNumber ) {
+  debugger
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
+
